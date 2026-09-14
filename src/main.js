48aa97dd68002bbf0a1522ca956273f27d9842ef
joacuba <jqcuba@gmail.com>
2026-09-14 11:32:04 -0500

    feat(ui): add primevue and theme configuration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import  Material from '@primeuix/themes/material';
+import '@primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
+const primeUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 createApp(App)
     .use(i18n)
+    .use(PrimeVue, { ripple: true, theme: { preset: Material }, license: primeUiLicenseKey })
     .mount('#app')
---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  VITE_PRIME_UI_LICENSE_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
